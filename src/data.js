b1d2e881adc71bb55218c25b556bbc13b705a8b2
RieBi <42877671+RieBi@users.noreply.github.com>
2024-08-03 07:52:22 +0200

    Add data

---
+++ src/data.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        "country": "Japan",
+        "image": "mount-fuji.png",
+        "mapsLink": "https://www.google.com/maps/place/Fud%C5%BEi/@35.3609498,138.7242378,2203m/data=!3m1!1e3!4m14!1m7!3m6!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!2zRnVkxb5p!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA!5m1!1e4?entry=ttu",
+        "title": "Mount Fuji",
+        "start_time": 1610409600,
+        "end_time": 1611446400,
+        "description": "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+    },
+    {
+        "country": "Australia",
+        "image": "sydney-opera-house.png",
+        "mapsLink": "https://www.google.com/maps/place/Opera+v+Sydney/@-33.8567844,151.2127218,561m/data=!3m2!1e3!4b1!4m6!3m5!1s0x6b12ae665e892fdd:0x3133f8d75a1ac251!8m2!3d-33.8567844!4d151.2152967!16zL20vMDZfbm0!5m1!1e4?entry=ttu",
+        "title": "Sydney Opera House",
+        "start_time": 1622073600,
+        "end_time": 1623110400,
+        "description": "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings"
+    },
+    {
+        "country": "Norway",
+        "image": "geirangerfjord.png",
+        "mapsLink": "https://www.google.com/maps/place/Geirangerfjord/@62.1047507,6.9098315,20218m/data=!3m2!1e3!4b1!4m6!3m5!1s0x46169d427b268c51:0xb8c99540dcc397fe!8m2!3d62.101506!4d7.0940817!16zL20vMDZ4eXFi!5m1!1e4?entry=ttu",
+        "title": "Geirangerfjord",
+        "start_time": 1633046400,
+        "end_time": 1637193600,
+        "description": "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
+    }
+]
